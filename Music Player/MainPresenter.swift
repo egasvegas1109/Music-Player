4975c5e831ec981f33ab70dcf6c711fab7daeeb4
EgasVegas <egor1109_45@mail.ru>
2022-05-13 14:04:21 +0300

    Presenter

---
+++ Music Player/MainPresenter.swift
@@ -0,0 +1,35 @@
+//
+//  MainPresenter.swift
+//  Music Player
+//
+//  Created by Егор Тютюкин on 13.05.2022.
+//
+
+import Foundation
+
+protocol MainViewProtocol: AnyObject{ //протокол для отправки
+    func setGreeting(greeting: String)
+}
+
+protocol MainViewpPresenterProtocol: AnyObject{ //протокол для приёма
+    init(view: MainViewProtocol, preson: Person)
+    func showGreeting()
+}
+
+class MainPresenter: MainViewpPresenterProtocol{
+    
+    required init(view: MainViewProtocol, preson: Person) {
+        self.view = view
+        self.person = preson
+    }
+    
+    func showGreeting() {
+        let greeting = self.person.firstName + " " + self.person.lastName
+        self.view.setGreeting(greeting: greeting)
+    }
+    
+    let view: MainViewProtocol
+    let person: Person
+    
+    
+}
---
+++ Music Player/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Music Player
+//
+//  Created by Егор Тютюкин on 13.05.2022.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
